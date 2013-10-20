72a4677944183ec8a9a8eb76ec6218dfdefb247e
manuel <manuel@Kenita-HP>
2013-10-19 20:47:14 -0600

    actualizacion del sistema

---
+++ CCC/src/ccc/CCC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccc;
+
+import Formularios.Inicio;
+
+/**
+ *
+ * @author manuel
+ */
+public class CCC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+		Inicio ini = new Inicio();
+		ini.setLocationRelativeTo(null);
+		ini.setVisible(true);
+
+		
+    }
+}
