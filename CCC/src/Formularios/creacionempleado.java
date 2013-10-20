b2876ef41b8ee5e52b91835a88eeec7494cc019f
Manuel <Manuel@Manuel-HP>
2013-10-20 00:22:52 -0600

    Creación de empleado

--- CCC/src/Formularios/creacionempleado.java
+++ CCC/src/Formularios/creacionempleado.java
@@ -390,7 +390,7 @@ public class creacionempleado extends javax.swing.JFrame {
     }//GEN-LAST:event_jRadioButton1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        if (txtnombre.getText().equals("")){
+ if (txtnombre.getText().equals("")){
 	JOptionPane.showMessageDialog(null, "Ingrese nombre");
         return;
             } 
@@ -421,37 +421,16 @@ if (txtnit.getText().equals("")){
 if (txtisss.getText().equals("")){
 	JOptionPane.showMessageDialog(null, "Ingrese número de ISSS");
     return;
-    }   
+    }
+	}
 
     /**
      * @param args the command line arguments
      */
-    //public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(creacionempleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(creacionempleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(creacionempleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(creacionempleado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+    public static void main(String args[]) {
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
             public void run() {
                 new creacionempleado().setVisible(true);
             }
