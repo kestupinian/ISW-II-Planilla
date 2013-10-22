b7d0f4af0e7469d7494c414d1185a2606b4d0a74
Manuel <Manuel@Manuel-HP>
2013-10-21 22:46:38 -0600

    Creación Clase para Conexión de la BD

--- CCC/src/Clases/cConexion.java
+++ CCC/src/Clases/cConexion.java
@@ -4,27 +4,51 @@
  */
 package Clases;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
+import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Manuel
+ * @author kaaf
  */
 public class cConexion {
-	private Connection cnx = null;
-    private Statement sttm = null;
-
-    public void UID(String sqlcad) {
-        try {
-            cnx = (Connection) DriverManager.getConnection("jdbc:mysql://root@localhost:3306/prueba");
-            sttm = (Statement) cnx.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            sttm.executeUpdate(sqlcad);
-        } catch (Exception e) {
-            System.out.println("ERROR=" + e.getMessage());
+  
+    public static Connection GetConnection()
+    {
+        Connection conexion=null;
+      
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            String servidor = "jdbc:mysql://localhost/prueba";
+            String usuarioDB="root";
+            String passwordDB="";
+            conexion= DriverManager.getConnection(servidor,usuarioDB,passwordDB);
+        }
+        catch(ClassNotFoundException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error1 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        catch(SQLException ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error2 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        catch(Exception ex)
+        {
+            JOptionPane.showMessageDialog(null, ex, "Error3 en la Conexión con la BD "+ex.getMessage(), JOptionPane.ERROR_MESSAGE);
+            conexion=null;
+        }
+        finally
+        {
+            return conexion;
         }
     }
 
+	public void close() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 }
