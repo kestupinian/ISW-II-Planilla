d27fb2fae131402b1ce2e75855f4c0b9ace84d99
Kenia Estupinian <kgestupim_717@hotmail.com>
2013-11-03 22:14:59 -0600

    clase empleado terminado

--- CCC/src/Clases/cEmpleado.java
+++ CCC/src/Clases/cEmpleado.java
@@ -13,8 +13,8 @@ public class cEmpleado {
 	cConexion1 c = new cConexion1();
 
         
-public void MODIFICAR (int codigo, String nombre, String apellido, String estado_civil, String fechanacimiento, String telefono, String correo, String direccion, String municipio, String departamento, String dui, String nit, String isss, String afp, String nup, String fechaingreso, String empresa, String nivel, String cargo, String sueldo){
- c.UID("UPDATE empleado set nombre='"+nombre+"',apellido='"+apellido+"', estado_civil= '"+estado_civil+"', fechanacimiento= '"+fechanacimiento+"', telefono= '"+telefono+"', correo= '"+correo+"', direccion= '"+direccion+"', municipio='"+municipio+"', departamento='"+departamento+"', dui='"+dui+"', nit='"+nit+"', isss='"+isss+"', afp='"+afp+"', nup='"+nup+"', fechaingreso='"+fechaingreso+"', empresa='"+empresa+"', nivel='"+nivel+"', cargo='"+cargo+"', sueldo='"+sueldo+"' where codigo ="+codigo);
+public void MODIFICAR (int codigo, String nombre, String apellido, String estado_civil, String telefono, String correo, String direccion, String municipio, String departamento, String dui, String nit, String isss, String afp, String nup, String empresa, String nivel, String cargo, String sueldo){
+ c.UID("UPDATE empleado set nombre='"+nombre+"',apellido='"+apellido+"', estado_civil= '"+estado_civil+"', telefono= '"+telefono+"', correo= '"+correo+"', direccion= '"+direccion+"', municipio='"+municipio+"', departamento='"+departamento+"', dui='"+dui+"', nit='"+nit+"', isss='"+isss+"', afp='"+afp+"', nup='"+nup+"', empresa='"+empresa+"', nivel='"+nivel+"', cargo='"+cargo+"', sueldo='"+sueldo+"' where codigo ="+codigo);
 }
 }
 
